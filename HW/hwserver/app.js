498e84803fe08999358a1679d27893dbcc058834
shpark <offsprings02@naver.com>
2020-02-10 17:50:34 +0900

    add ngrok status

--- HW/hwserver/app.js
+++ HW/hwserver/app.js
@@ -3,6 +3,7 @@ const cron = require('node-cron');
 const app = express();
 const port = 3000;
 const Funcs = require('./routes/logupdate/logupdatecontroller.js');
+const ngrok_info = require('./ngrok_info/ngrok_info.js');
 
 app.use('/', require('./routes/index.js'));
 
@@ -10,6 +11,8 @@ cron.schedule('0,30 * * * *', function(){
     Funcs.selectAll();
     console.log("CRON ACCEPT");
 });
+
+ngrok_info.ngrok_info();
 
 app.listen(port, () => {
     console.log(`server is listening at localhost:${port}`);
